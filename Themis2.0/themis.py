62e62917436f3354a1f7f421678b5e6d4841d389
Rico Angell <rangell@umass.edu>
2017-10-03 10:57:38 -0400

    added more documentation

--- Themis2.0/themis.py
+++ Themis2.0/themis.py
@@ -15,11 +15,23 @@ import xml.etree.ElementTree as ET
 
 class Input:
     """
+    Class to define an input characteristic to the software.
+
+    Attributes
+    ----------
+    name : str
+        Name of the input.
+    values : list
+        List of the possible values for this input.
+
+    Methods
+    -------
+    get_random_input()
+        Returns a random element from the values list.
     """
     def __init__(self, name="", values=[]):
         self.name = name
         self.values = [str(v) for v in values]
-        self.num_values = len(values)
 
     def get_random_input(self):
         """
@@ -39,6 +51,25 @@ class Input:
 
 class Test:
     """
+    Data structure for storing tests.
+
+    Attributes
+    ----------
+    function : str
+        Name of the function to call
+    i_fields : list of `Input.name`
+        The inputs of interest, i.e. compute the casual discrimination wrt
+        these fields.
+    threshold : float in [0,1]
+        At least this level of discrimination to be considered.
+    conf : float in [0, 1]
+        The z* confidence level (percentage of normal distribution.
+    margin : float in [0, 1]
+        The margin of error for the confidence.
+    group : bool
+        Search for group discrimination if `True`.
+    causal : bool
+        Search for causal discrimination if `True`.
     """
     def __init__(self, function="", i_fields=[], conf=0.999, margin=0.0001,
                     group=False, causal=False, threshold=0.2):
@@ -52,12 +83,14 @@ class Test:
 
     def __str__(self):
         s = "Test: " + self.function + "\n"
-        s += "Characteristics: " + ", ".join(self.i_fields) + "\n"
+        if self.function != "discrimination_search":
+            s += "Characteristics: " + ", ".join(self.i_fields) + "\n"
+        else:
+            s += "Threshold: " + str(self.threshold) + "\n"
+            s += "Group: " + str(self.group) + "\n"
+            s += "Causal: " + str(self.causal) + "\n"
         s += "Confidence: " + str(self.conf) + "\n"
         s += "Margin: " + str(self.margin) + "\n"
-        s += "Threshold: " + str(self.threshold) + "\n"
-        s += "Group: " + str(self.group) + "\n"
-        s += "Causal: " + str(self.causal) + "\n"
         return s
 
     __repr__ = __str__
@@ -65,8 +98,14 @@ class Test:
 
 class Themis:
     """
-    """
+    Compute discrimination for a piece of software.
 
+    Attributes
+    ----------
+
+    Methods
+    -------
+    """
     def __init__(self, xml_fname=""):
         """
         Initialize Themis from xml file.
@@ -88,48 +127,19 @@ class Themis:
         self.command = root.find("command").text.strip()
         self._build_input_space(args=root.find("inputs"))
         self._load_tests(args=root.find("tests"))
-        self.cache = {}
-
-    def _build_input_space(self, args=None):
-        assert args != None
-        self.inputs = {}
-        self.input_order = []
-        for obj in args.findall("input"):
-            name = obj.find("name").text
-            values = []
-            t = obj.find("type").text
-            if t == "categorical":
-                values = [elt.text for elt in obj.find("values").findall("value")]
-            elif t == "continuousInt":
-                values = range(int(obj.find("bounds").find("lowerbound").text),
-                               int(obj.find("bounds").find("upperbound").text)+1)
-            else:
-                assert false
-            self.inputs[name] = Input(name=name, values=values)
-            self.input_order.append(name)
-
-    def _load_tests(self, args=None):
-        assert args != None
-        self.tests = []
-        for obj in args.findall("test"):
-            test = Test()
-            test.function = obj.find("function").text
-            if test.function == "causal_discrimination" or \
-               test.function == "group_discrimination":
-                test.i_fields = [elt.text 
-                        for elt in obj.find("i_fields").findall("input_name")]
-            if test.function == "discrimination_search":
-                test.group = bool(obj.findall("group"))
-                test.causal = bool(obj.findall("causal"))
-                test.threshold = float(obj.find("threshold").text)
-            test.conf = float(obj.find("conf").text)
-            test.margin = float(obj.find("margin").text)
-            self.tests.append(test)
+        self._cache = {}
 
     def run(self):
         """
-        Run Themis given the configuration.
+        Run Themis tests specified in the configuration file.
         """
+        print "\nTHEMIS 2.0"
+        print "----------"
+        print "SOFTWARE NAME: " + self.software_name
+        print "MAX SAMPLES: " + str(self.max_samples)
+        print "MIN SAMPLES: " + str(self.min_samples)
+        print "COMMAND: " + self.command
+        print "RANDOM SEED: " + str(self.rand_seed)
         for test in self.tests:
             random.seed(self.rand_seed)
             print "--------------------------------------------------"
@@ -157,78 +167,13 @@ class Themis:
                     print "-----"
                     for key, value in g.items():
                         print ", ".join(key) + " --> " + str(value)
-                print "\n"
+                    print ""
                 if c:
                     print "Causal"
                     print "------"
                     for key, value in c.items():
                         print ", ".join(key) + " --> " + str(value)
         print "--------------------------------------------------"
-
-    def new_random_sub_input(self, args=[]):
-        """
-        Produce a random input for the each of the elements of `attributes`.
-
-        Parameters
-        ----------
-        attributes : list of str
-            list of attribute names that we want random assignments to.
-
-        Returns
-        -------
-        dict
-            the assigns to each of the attributes.
-        """
-        assert args
-        return {name : self.inputs[name].get_random_input() for name in args}
-
-    def gen_all_sub_inputs(self, args=[]):
-        """
-        Produces a list of all possible assignments.
-
-        Parameters
-        ----------
-        attributes : list of str
-            list of attribute names that we want random assignments to.
-
-        Returns
-        -------
-        generator of dict
-            generator for the assignments.
-        """
-        assert args
-        vals_of_args = [self.inputs[arg].values for arg in args]
-        combos = [list(elt) for elt in list(product(*vals_of_args))]
-        return ({arg : elt[idx] for idx, arg in enumerate(args)} \
-                                                 for elt in combos)
-
-    def get_test_result(self, assign=None):
-        """
-        Returns the output of the software with input `assign`.
-
-        Checks the cache to see if the output for `assign` has already
-        been computed. If it has, return the cached results. Otherwise, run
-        the software and cache and return the result.
-
-        Parameters
-        ----------
-        assign : dict
-            dictionary of all input assignments.
-
-        Returns
-        -------
-        bool
-            the output of the software.
-        """
-        assert assign != None
-        tupled_args = self._tuple(assign)
-        if tupled_args in self.cache.keys():
-            return self.cache[tupled_args]
-
-        cmd = self.command + " " + " ".join(tupled_args)
-        output = commands.getoutput(cmd).strip()
-        self.cache[tupled_args] = (commands.getoutput(cmd).strip() == "1")
-        return self.cache[tupled_args]
 
     def group_discrimination(self, i_fields=None, conf=0.999, margin=0.0001):
         """
@@ -238,7 +183,7 @@ class Themis:
         ----------
         i_fields : list of `Input.name`
             The inputs of interest, i.e. compute the casual discrimination wrt
-            these fiels.
+            these fields.
         conf : float in [0, 1]
             The z* confidence level (percentage of normal distribution.
         margin : float in [0, 1]
@@ -255,13 +200,13 @@ class Themis:
         assert i_fields != None
         min_group, max_group, test_suite, p = float("inf"), 0, [], 0
         rand_fields = self._all_other_fields(i_fields)
-        for fixed_sub_assign in self.gen_all_sub_inputs(args=i_fields):
+        for fixed_sub_assign in self._gen_all_sub_inputs(args=i_fields):
             count = 0
             for num_sampled in range(1, self.max_samples):
-                assign = self.new_random_sub_input(args=rand_fields)
+                assign = self._new_random_sub_input(args=rand_fields)
                 assign.update(fixed_sub_assign)
                 self._add_assignment(test_suite, assign)
-                count += self.get_test_result(assign=assign)
+                count += self._get_test_result(assign=assign)
 
                 p, end = self._end_condition(count, num_sampled, conf, margin)
                 if end:
@@ -280,7 +225,7 @@ class Themis:
         ----------
         i_fields : list of `Input.name`
             The inputs of interest, i.e. compute the casual discrimination wrt
-            these fiels.
+            these fields.
         conf : float in [0, 1]
             The z* confidence level (percentage of normal distribution.
         margin : float in [0, 1]
@@ -298,17 +243,17 @@ class Themis:
         count, test_suite, p = 0, [], 0
         f_fields = self._all_other_fields(i_fields) # fixed fields
         for num_sampled in range(1, self.max_samples):
-            fixed_assign = self.new_random_sub_input(args=f_fields)
-            singular_assign = self.new_random_sub_input(args=i_fields)
+            fixed_assign = self._new_random_sub_input(args=f_fields)
+            singular_assign = self._new_random_sub_input(args=i_fields)
             assign = self._merge_assignments(fixed_assign, singular_assign)
             self._add_assignment(test_suite, assign)
-            result = self.get_test_result(assign=assign)
-            for dyn_sub_assign in self.gen_all_sub_inputs(args=i_fields):
+            result = self._get_test_result(assign=assign)
+            for dyn_sub_assign in self._gen_all_sub_inputs(args=i_fields):
                 if dyn_sub_assign == singular_assign:
                     continue
                 assign.update(dyn_sub_assign)
                 self._add_assignment(test_suite, assign)
-                if self.get_test_result(assign=assign) != result:
+                if self._get_test_result(assign=assign) != result:
                     count += 1
                     break
 
@@ -341,7 +286,7 @@ class Themis:
 
         Returns
         -------
-        tuple of list of list - (group_subsets, causal_subsets)
+        tuple of dict
             The lists of subsets of the input characteristics that discriminate.
         """
         assert group or causal
@@ -377,6 +322,28 @@ class Themis:
             if not next_subset:
                 return True
 
+    def _new_random_sub_input(self, args=[]):
+        assert args
+        return {name : self.inputs[name].get_random_input() for name in args}
+
+    def _gen_all_sub_inputs(self, args=[]):
+        assert args
+        vals_of_args = [self.inputs[arg].values for arg in args]
+        combos = [list(elt) for elt in list(product(*vals_of_args))]
+        return ({arg : elt[idx] for idx, arg in enumerate(args)} \
+                                                 for elt in combos)
+
+    def _get_test_result(self, assign=None):
+        assert assign != None
+        tupled_args = self._tuple(assign)
+        if tupled_args in self._cache.keys():
+            return self._cache[tupled_args]
+
+        cmd = self.command + " " + " ".join(tupled_args)
+        output = commands.getoutput(cmd).strip()
+        self._cache[tupled_args] = (commands.getoutput(cmd).strip() == "1")
+        return self._cache[tupled_args]
+
     def _add_assignment(self, test_suite, assign):
         if assign not in test_suite:
             test_suite.append(assign)
@@ -397,7 +364,6 @@ class Themis:
         merged.update(assign1)
         merged.update(assign2)
         return merged
-        return False
 
     def _tuple(self, assign=None):
         assert assign != None
@@ -408,6 +374,43 @@ class Themis:
         listed_args = list(tupled_args)
         return {name : listed_args[idx] \
                     for idx, name in enumerate(self.input_order)}
+
+    def _build_input_space(self, args=None):
+        assert args != None
+        self.inputs = {}
+        self.input_order = []
+        for obj in args.findall("input"):
+            name = obj.find("name").text
+            values = []
+            t = obj.find("type").text
+            if t == "categorical":
+                values = [elt.text
+                            for elt in obj.find("values").findall("value")]
+            elif t == "continuousInt":
+                values = range(int(obj.find("bounds").find("lowerbound").text),
+                             int(obj.find("bounds").find("upperbound").text)+1)
+            else:
+                assert false
+            self.inputs[name] = Input(name=name, values=values)
+            self.input_order.append(name)
+
+    def _load_tests(self, args=None):
+        assert args != None
+        self.tests = []
+        for obj in args.findall("test"):
+            test = Test()
+            test.function = obj.find("function").text
+            if test.function == "causal_discrimination" or \
+               test.function == "group_discrimination":
+                test.i_fields = [elt.text
+                        for elt in obj.find("i_fields").findall("input_name")]
+            if test.function == "discrimination_search":
+                test.group = bool(obj.findall("group"))
+                test.causal = bool(obj.findall("causal"))
+                test.threshold = float(obj.find("threshold").text)
+            test.conf = float(obj.find("conf").text)
+            test.margin = float(obj.find("margin").text)
+            self.tests.append(test)
 
 
 if __name__ == '__main__':
